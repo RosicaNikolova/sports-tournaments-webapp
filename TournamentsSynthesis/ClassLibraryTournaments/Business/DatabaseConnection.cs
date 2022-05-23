417ad68a629a6fdeb98690a9f0d188a642fbd946
RosicaNikolova <rosicanikolova1402@gmail.com>
2022-05-23 12:18:16 +0200

    Database Connection class created

---
+++ TournamentsSynthesis/ClassLibraryTournaments/Business/DatabaseConnection.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryTournaments.Business
+{
+    public class DatabaseConnection
+    {
+        public static MySqlConnection CreateConnection()
+        {
+            return new MySqlConnection("Server=studmysql01.fhict.local;Uid=dbi497286;Database=dbi497286;Pwd=9Rosica9");
+        }
+    }
+}
